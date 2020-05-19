b5ec6fcbe19aa1eb8e47721e55bcce848e3a70e3
felix <felix@PC-Felix.fritz.box>
2020-05-19 17:10:38 +0200

    123

---
+++ objekte/MonteurAuftragsTabelle.java
@@ -0,0 +1,12 @@
+package objekte;
+
+import java.util.ArrayList;
+
+public class MonteurAuftragsTabelle extends AuftragsTabelle {
+
+	public MonteurAuftragsTabelle(ArrayList<Auftrag> auftragsListe) {
+		super(auftragsListe);
+		// TODO Auto-generated constructor stub
+	}
+
+}
