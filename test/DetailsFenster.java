2626116dc975b025414cf5224a73490d1eeaf5dc
AndiOnn <AndiOnn@XMG-A507>
2020-05-22 13:14:11 +0200

    Mein GUI Test

---
+++ test/DetailsFenster.java
@@ -0,0 +1,53 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableCellRenderer;
+
+public class DetailsFenster extends JFrame implements TableCellRenderer {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DetailsFenster frame = new DetailsFenster();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DetailsFenster() {
+		setTitle("Auftragsdetails");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//Nur dieses Fenster wird Geschlossen
+		setBounds(100, 100, 468, 282);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
