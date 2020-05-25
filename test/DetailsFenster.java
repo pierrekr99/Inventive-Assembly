412efffb020e211ac5e45ddab49bb81c9e1e0045
jani6 <jani6@LAPTOP-C35C1HGA>
2020-05-25 11:55:00 +0200

    DetailsFenster wurde eingefügt (ohne Inhalt)

--- test/DetailsFenster.java
+++ test/DetailsFenster.java
@@ -3,17 +3,34 @@ package test;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
 public class DetailsFenster extends JFrame implements TableCellRenderer {
 
 	private JPanel contentPane;
-
+	private JTable tKomponenten;
+	
+	
+	int zeilen = 5;// Aufragliste.size; Die Anzahl der Zeilen, die die Tabelle hat
+	int zeile = 0;// Zeile in der der Neue Auftrag eingefügt wird
+	
+	Object[][] komponenten = new Object[zeilen][6];// Nur das wird später eingelesen
+	String[] komponente = new String[4];// Ein Auftrag wird als Zeile erstellt (Zeile mit 4 Spalten)
+	
+	
+//	datenbankVerbindung db = new datenbankVerbindung();
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -36,11 +53,43 @@ public class DetailsFenster extends JFrame implements TableCellRenderer {
 	public DetailsFenster() {
 		setTitle("Auftragsdetails");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//Nur dieses Fenster wird Geschlossen
-		setBounds(100, 100, 468, 282);
+		setBounds(100, 100, 800, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JScrollPane sPAuftraege = new JScrollPane();
+
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addComponent(sPAuftraege, GroupLayout.DEFAULT_SIZE, 1574, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(31)
+					.addComponent(sPAuftraege, GroupLayout.DEFAULT_SIZE, 819, Short.MAX_VALUE))
+		);
+		
+//		komponenten();
+		tKomponenten = new JTable();// Neue Tabelle
+		tKomponenten.setCellSelectionEnabled(true);// Einzelne Zellen können ausgewählt werden
+		tKomponenten.setFont(new Font("Tahoma", Font.PLAIN, 16));// Schriftart und -größe in der Tabelle
+		tKomponenten.getTableHeader().setFont(new Font("Tahoma", Font.PLAIN, 20));// Schriftart und -größe in der
+																				// Kopfzeile der Tabelle
+		tKomponenten.setModel(new DefaultTableModel(komponenten, // Benötigter Inhalt: (String[][],String[])
+				// Sonst wird hier ein eigenes Modell Eingefügt
+				new String[] { "TeileNr.", "Name", "Kategorie", "Verfügbarkeit", }));// Generierung der
+																									// Tabelle
+		sPAuftraege.setViewportView(tKomponenten);
+
+		JPanel panel = new JPanel();
+		sPAuftraege.setColumnHeaderView(panel);
+		contentPane.setLayout(gl_contentPane);
+		
+		
 	}
 
 	@Override
